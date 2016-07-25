a530cdd08cb4c33be4fde810fcd939b9eb3208e5
TrRo26 <travis_roy@yahoo.com>
2016-07-25 01:26:22 -0500

    Add nested data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,27 @@
+
+nordic = {
+	"Sweden" => [
+		"Stockholm",
+		"Gothenburg",
+		"Malmo"
+		],
+	"Norway" => [
+		"Oslo",
+		"Lillehammer",
+		"Bergen"
+		],
+	"Denmark" => [
+		"Copenhagen",
+		"Aarhus",
+		"Odense"
+		],
+	"Finland" => [
+		"Helsinki",
+		"Turku",
+		"Tampere"
+		]	
+}
+
+p nordic["Sweden"][0]
+
+p nordic["Finland"][2]
