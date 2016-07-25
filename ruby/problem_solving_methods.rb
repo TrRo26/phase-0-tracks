5a4f519f3553585cbb388fe1cb02fe183f8e47c0
TrRo26 <travis_roy@yahoo.com>
2016-07-25 10:38:49 -0500

    Add 5.6 problem solving methods

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,33 @@
+# 5.6 Manditory Pairing
+
+# RELEASE 0
+
+=begin
+integers = [4, 7, 1, 14, 47, 5]
+
+def my_method(array, srchnum)
+	#total = array.length
+	total = 0
+	array.each do |x|
+		if x == srchnum
+			puts total
+		else 
+			total += 1
+		end
+	end
+end
+=end
+
+
+
+#RELEASE 1
+
+def fib(input)
+	prev1 = 0
+	prev2 = 1
+	sum = prev1 + prev2
+
+	while input.index 
+
+#RELEASE 2
+
