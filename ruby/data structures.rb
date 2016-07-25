bfb63f1714f7883abb3ef7148cd6dfba108d3114
TrRo26 <travis_roy@yahoo.com>
2016-07-25 02:05:42 -0500

    Add week 5 review and reflect

---
+++ ruby/data structures.rb
@@ -0,0 +1,33 @@
+Data Structures
+1. Why are data structures useful in programming?
+	They allow for hundreds and even millions of variables and organize data in a way that is accessable and sortable if needed. They allow for additional methods of indexing as well for ease of recall.
+
+Arrays
+2. Define "array" and describe the characteristics of an array.
+	An array is a collection of objects that are accessable by a (automatically) assigned number index. An array can contain integers or strings or a combination. 
+
+3. Give two original examples of data sets that an array might be well-suited for.
+	(1) types of fruit
+	(2) list of each playing card within a deck
+
+Hashes
+4. Define "hash" and describe the characteristics of a hash.
+	A hash is somewhat similar to an array, but allows a pairing between key names and values. The keys are singular and can only be used once, but the same assigned values can be used numerous time for differnt keys.
+
+5. Give two original examples of data sets that a hash might be well-suited for.
+	(1) Names of students and their accompanying test scores
+	(2) Products and their associated prices
+
+Iteration
+6. Why is iteration useful?
+	It allows for the automation of repetitive processes while updating the process on every cycle through. Without iteration, you would potentially have to write out 100's of variations of something that would otherwise only have to be written once with the use of iteration. 
+
+7. Give an example of a real-life task that would require iteration if you wrote an algorithm for it.
+	Shopping for clothes
+
+Blocks
+8. What is a block?
+	A block is a collection of code denoted by "do" and "end" or {  }, for inline.
+
+9. What is a block parameter, and where does its value come from?
+	A block parameter is one instance of an item in an array. This changes, or cycles to the next item, on every iteration through the block. The variable of the block parameter can be selected arbitrarily, if need be.
