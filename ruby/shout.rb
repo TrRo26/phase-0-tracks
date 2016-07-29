7ad04aa4f006e2e2f7f046310e7334194dd09edc
TrRo26 <travis_roy@yahoo.com>
2016-07-29 16:15:28 -0500

    Complete all releases for 6.4 Modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,38 @@
+
+# Make a mixin version of the Shout module
+# Write two classes representing anything that might shout, and include the Shout module in those classes
+# Test your work by adding driver code at the bottom of the file that instantiates instances of your classes 
+#   and calls the two module methods on each instance.
+
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + "!! Yay!"
+	end
+end
+
+class Audiences
+	include Shout
+end
+
+class Mobs
+	include Shout
+end
+
+audience = Audiences.new
+puts audience.yell_happily("So good, Encore")
+puts audience.yell_angrily("Your performance sucked")
+
+mob = Mobs.new
+puts mob.yell_angrily("Don't even tell us you're out of fried chicken")
+puts mob.yell_happily("You do have more fried chicken")
+
+__END__
+
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
