46a983579ec5ec2afcdc9ef25b488298a9f0162f
TrRo26 <travis_roy@yahoo.com>
2016-07-30 12:25:01 -0500

    Complete 6.5 pairing session - todolist.rb

---
+++ ruby/list/todolist.rb
@@ -0,0 +1,26 @@
+class TodoList
+
+	def initialize(item)
+		@list = item
+	end
+
+	def get_items
+		@list
+	end
+
+	def add_item(item)
+		@list << item
+	end
+
+	def delete_item(item)
+		@list.delete(item)
+	end
+
+	def get_item(index)
+		@list[index]
+	end
+
+end
+
+test = TodoList.new("test1")
+p test.get_items
