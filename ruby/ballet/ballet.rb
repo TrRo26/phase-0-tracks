a06bbc1597236d5fcbc561261fe697e03c7c5916
TrRo26 <travis_roy@yahoo.com>
2016-08-01 09:43:50 -0500

    Finish practice assessment question 4 out of 4

--- ruby/ballet/ballet.rb
+++ ruby/ballet/ballet.rb
@@ -5,6 +5,8 @@ class Dancer
 	def initialize(name, age)
 		@name = name.to_s
 		@age = age.to_i
+		@card = []
+		@sl_col = "white"
 	end
 
 	def name
@@ -13,6 +15,31 @@ class Dancer
 
 	def age
 		@age
+	end
+
+	def pirouette
+		p "*twirls*"
+	end
+
+	def bow
+		p "*bows*"
+	end
+
+	def queue_dance_with(partner)
+		@card << partner
+	end
+
+	def card
+		@card
+	end
+
+	def begin_next_dance
+		"Now dancing with #{@card.delete_at(0)}."
+	end
+
+	def slipper_color(color)
+		@sl_col = color
+		p "The color of ballet slippers have been changed to #{color}."
 	end
 
 end
