54f5eb134b092c7416eb83fa0147504ae0f1fe31
TrRo26 <travis_roy@yahoo.com>
2016-08-10 00:55:00 -0400

    Add partially completed release 2

--- js/algos.js
+++ js/algos.js
@@ -15,6 +15,11 @@ REQUIREMENTS
 	- Add some driver code that tests both outcomes of your function.
 
 	RELEASE 2
+	- Write a function that takes an integer for length, and builds and returns an 
+	  array of strings of the given length.
+	- Add driver code that does the following 10 times: generates an array, 
+	  prints the array, feeds the array to your "longest word" function, and prints 
+	  the result.
 
 */
 // ==============================================================================
@@ -49,20 +54,25 @@ function Lw(array) {
 
 // Create function that compares key-value pairs and returns true if any match
 function Share(objOne, objTwo) {
+	// Print objects
 	console.log("");
 	console.log("Objects being compared:", objOne, objTwo);
 	console.log("");
 
+	// Assign object key names to an array
 	var key1 = Object.keys(objOne);
 	var key2 = Object.keys(objTwo);
 
+	// Initiate loop and determine number of times to loop
 	for (var i = 0; i < key1.length; i++) {
+		// Compare key-value pairs of Object using arrays
 		if (objOne[key1[i]] == objTwo[key1[i]]) {
+		// Assign boolean for result of each comparison
 			var final1 = true;
 		} else { 
 			var final2 = false;
 		}
-
+		// If any matches were found return true, otherwise false
 		if (final1 || final2 == true) {
 			console.log("Found a match: value =", true)
 		} else { 
@@ -72,15 +82,58 @@ function Share(objOne, objTwo) {
 	}
 }
 
+// Create function that builds array of strings of varying length
+function BldArr(int) {
+	// Assign random number between 1 and 26 to variable
+	var randAlph = (Math.floor((Math.random() * 26) + 1));
+	// Assign random number between 1 and 10 to variable
+	var randWL = (Math.floor((Math.random() * 10) + 1));
+	var alph = "abcdefghijklmnopqrstuvwxyz";
+	var strings = []
+	var word = ""
+
+	// Loop through alphabet random number of times between 1 and 10, choosing a letter each time
+
+	/*
+	For (var i = 0, i < int, i++) {
+		For (var i = 0, i < randWL, i++) {
+			var word = randAlph + randAlph
+		}
+	}
+*/
+	console.log(randWL)
+	console.log(alph[randAlph])
+	
+	//alph[randAlph]
+}
+
+
+/*
+- Write a function that takes an integer for length, and builds and returns an 
+array of strings of the given length. 
+So if we ran your function with an argument of 3, we would get an array of 
+3 random words back (the words don't have to be actual sensical English words 
+-- "nnnnfph" totally counts). The words should be of randomly varying length, 
+with a minimum of 1 letter and a maximum of 10 letters. (This involves a new 
+trick, generating a random number, that you'll have to look up, but the solution 
+on how to do so is relatively straightforward.)
+
+- Add driver code that does the following 10 times: generates an array, 
+prints the array, feeds the array to your "longest word" function, and prints 
+the result.
+
+*/
+
 // ==============================================================================
 // DRIVER CODE
 
-var collection1 = {artist: 'Beatles', popularity: 'low'};
-var collection2 = {artist: 'Rolling Stones', popularity: 'high'}
+// RELEASE 2
 
-var test = new Share(collection1, collection2)
+what = new BldArr(3)
 
-/*
+
+/* 
+RELEASE 0
 
 // Create arrays for release 0
 var phrases1 = ["Hi!", "Hello there!", "How are you today!", "Salutations!"];
@@ -97,12 +150,18 @@ nonsense.find()
 var numbers = new Lw(phrases3)
 numbers.find()
 
+
+RELEASE 1
+
+// Define two Objects with multiple key-value pairs to compare
+var collection1 = {artist: 'Beatles', popularity: 'low'};
+var collection2 = {artist: 'Rolling Stones', popularity: 'high'}
+
+// Create a new instance of Share to test the function
+var test = new Share(collection1, collection2)
+
 */
 
 // ==============================================================================
-// TEMPORARY/REFERENCE CODE
-/*
-
-*/
 
 
