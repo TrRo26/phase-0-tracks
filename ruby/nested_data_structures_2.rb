73efab4e9b0ea654568abe6ccc2e9fafdd1ebcb3
TrRo26 <travis_roy@yahoo.com>
2016-08-14 14:53:12 -0500

    Add nested data structure with specific calls

---
+++ ruby/nested_data_structures_2.rb
@@ -0,0 +1,21 @@
+books = {
+	"Fiction" => [
+		"A Farewell to Arms",
+		"Catch-22",
+		"Ham on Rye"
+		],
+	"Non-Fiction" => [
+		"Freakonomics",
+		"Fatal Shore",
+		"The ABC's of the Bible"
+		],
+	"Instructional" => [
+		"The Complete Juggler",
+		"Worlds Best Card Tricks",
+		"30 Days to Great French"
+		],
+}
+
+p books["Fiction"][0]
+p books["Instructional"][2]
+p books.values[0][1]
