ea07ea8e4f7783b110c88ea4f873e0c8902e18dc
TrRo26 <travis_roy@yahoo.com>
2016-08-17 20:07:02 -0500

    Add Reflection comments to GPS 2.2

--- ruby/gps2_2_2.rb
+++ ruby/gps2_2_2.rb
@@ -83,4 +83,38 @@ end
 
 print_list(wed_list)
 
+# ==============================
+# REFLECTION & COMMENTS
+# ==============================
+=begin
 
+- What did you learn about pseudocode from working on this challenge?
+	For the first time, I finially realized how useful pseudocoding can be. It was really
+	helpful to think through the entire problem before diving in. While I've always tried to
+	break things down into smaller problems first, pseudocoding really prevents you from
+	skipping over vital parts in your coding.
+
+- What are the tradeoffs of using arrays and hashes for this challenge?
+	Arrays are often a bit easier to manipulate while hashes may be a bit more complex, they
+	provide the very usuful function of assigning key-value pairs.
+
+- What does a method return?
+	That depends on the method. Implicitly, it returns the value of the last line of code.
+	If this is a calculation, it might be an integer, if it is a string, it would be a string.
+	If using 'puts' it will return 'nil'. You can control this by using the keyword "return".
+
+- What kind of things can you pass into methods as arguments?
+	Just about anything: strings, integers, booleans, variables, arrays, hashes, other methods
+	etc.
+	
+- How can you pass information between methods?
+	You can pass information between methods by passing another method through the current
+	method as an argument.
+
+- What concepts were solidified in this challenge, and what concepts are still confusing?
+	I had struggled significantly with the concept of passing information bewtween methods
+	without using global or class structure. I feel this challenge really brough things together
+	for me and really increased my confidence and understanding of how methods can interact
+	on the most basic level.
+
+=end
