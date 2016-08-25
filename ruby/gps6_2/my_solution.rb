a792e169c66f7ac92cf799bcf4a50c6507801703
TrRo26 <travis_roy@yahoo.com>
2016-08-24 23:06:41 -0500

    Complete through release 7

--- ruby/gps6_2/my_solution.rb
+++ ruby/gps6_2/my_solution.rb
@@ -20,18 +20,19 @@ class VirusPredictor
     @population_density = population_density
   end
 
+  
   # combines the printed results of predicted_deaths and speed_of_spread.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
   # calculates the predicted deaths by population density and population of the state.
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
+    if @population_density >= 200  # population_density/500 = multiplier
       number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
       number_of_deaths = (@population * 0.3).floor
@@ -48,7 +49,7 @@ class VirusPredictor
   end
 
   # calculates the speed of spread based on population density.
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread  #(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
